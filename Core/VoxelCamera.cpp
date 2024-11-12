35f1bdd45e0a95b8610c33cc6d65818aaa71f9de
Michael Mason <michaelmason@live.com.au>
2024-11-12 16:08:37 -0500

    create voxel camera

---
+++ Core/VoxelCamera.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "VoxelCamera.h"
+
+void VoxelCamera::UpdateMatrix()
+{
+
+	Update(); 
+}
---
+++ Core/VoxelCamera.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Camera.h"
+#include "VectorMath.h"
+
+class VoxelCamera : public Math::BaseCamera
+{
+
+	VoxelCamera() {}
+
+	void UpdateMatrix(); 
+
+	
+};
+
